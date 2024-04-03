656f5f1faa370d0ef75b1a2744fe604b765cde31
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-04-03 14:35:27 +0800

    fix: type

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { selectAll, insertUser } from "@/functions/user";
 import { SelectUser } from "@/db/schema";
 
-export async function GET(request: Request): NextResponse {
+export async function GET(request: Request) {
   const users = await selectAll();
   return NextResponse.json(users);
 }
